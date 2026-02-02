099229382cb1ec17be8b2f244b89221930185be6
Sunly <sunly.chao@rexform.io>
2026-02-02 15:04:57 +0700

    Config migration input

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo " Starting Ory Keto..."
 
 echo " Running database migrations..."
-keto migrate up -c /etc/keto/keto.yml
+keto migrate up -c /etc/keto/keto.yml --yes
 
 echo " Starting Keto server..."
 exec keto serve -c /etc/keto/keto.yml
