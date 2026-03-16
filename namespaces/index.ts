10303de25d8e1732b8af1dc81a52239ee94a0ef9
Sunly <sunly.chao@rexform.io>
2026-03-16 11:04:26 +0700

    config namespace entrypoint

---
+++ namespaces/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Keto namespace entrypoint.
+ *
+ * Keto loads this file and resolves imported namespace classes.
+ * Keeping a single entrypoint avoids runtime ambiguity when loading
+ * namespaces from a directory path.
+ */
+
+export { User } from "./user";
+export { GlobalRole } from "./globalRole";
+export { Organization, OrgGroup, OrgRole } from "./organization";
+export { Group } from "./group";
+export { Role } from "./role";
+export { RoleBinding } from "./roleBinding";
+export { GitlabGroup } from "./gitlabGroup";
+export { GitlabProject } from "./gitlabProject";
